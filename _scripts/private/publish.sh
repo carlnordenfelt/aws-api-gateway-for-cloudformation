cbffef1e1dd866f891f0090c3d27d5f8da6a1b65
Carl Nordenfelt <carl@nordenfelt.com>
2016-02-21 15:02:48 +0100

    unix installation scripts

--- _scripts/private/publish.sh
+++ _scripts/private/publish.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 npm run clean
 npm run test
@@ -26,6 +26,8 @@ echo "Packaging version: ${version}"
 echo "Packaging source code"
 zip -r package.zip lib/* lib/*/** node_modules/*/** package.json > /dev/null 2>&1
 aws s3 cp package.zip s3://apigatewaycloudformation/builds/${version}.zip
+aws s3api put-object --bucket apigatewaycloudformation --key builds/latest --website-redirect-location /builds/${version}.zip --content-type text/html
+
 rm -rf package.zip
 
 echo "Packaging installation files"
@@ -38,6 +40,7 @@ fi
 
 zip -r install-package.zip ./* ./*/** > /dev/null 2>&1
 aws s3 cp install-package.zip s3://apigatewaycloudformation/install/${version}.zip
+aws s3api put-object --bucket apigatewaycloudformation --key install/latest --website-redirect-location /install/${version}.zip --content-type text/html
 rm -rf install-package.zip
 
 echo "Done installing version ${version}"
