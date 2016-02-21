b61b64a9eed14996c29660335bb310c3584333e5
Carl Nordenfelt <carl@nordenfelt.com>
2016-02-21 15:38:18 +0100

    separate publish script

--- _scripts/private/publish.sh
+++ _scripts/private/publish.sh
@@ -2,7 +2,7 @@
 
 version=$1
 
-aws s3api put-object --bucket apigatewaycloudformation --key builds/latest --website-redirect-location /builds/${version}.zip --content-type text/html
-aws s3api put-object --bucket apigatewaycloudformation --key install/latest --website-redirect-location /install/${version}.zip --content-type text/html
+aws s3api put-object --bucket apigatewaycloudformation --key builds/latest --website-redirect-location /builds/v${version}.zip --content-type text/html
+aws s3api put-object --bucket apigatewaycloudformation --key install/latest --website-redirect-location /install/v${version}.zip --content-type text/html
 
 echo "${version} published"
