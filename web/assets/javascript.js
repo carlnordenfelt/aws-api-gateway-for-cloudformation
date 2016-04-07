cd57467460e0e0addef59edf50e8ce1303422134
Carl Nordenfelt <carl@nordenfelt.com>
2016-04-07 14:50:27 +0200

    Moved documentation to external site

---
+++ web/assets/javascript.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    $('body').on('click', '.scrollspy a', function (e) {
+        e.preventDefault();
+        var target = $(this).attr('href');
+        $(window).scrollTop($(target).offset().top - 60);
+    });
+    $('#nav').affix({
+        offset: {
+            top: 0
+        }
+    });
+
+    $('body').on('click', 'span[data-collapse]', function () {
+        $(this).closest('section').find('.panel-body').toggle();
+        $(this).closest('section').find('.panel-footer').toggle();
+        $(this).toggleClass('glyphicon-chevron-right');
+        $(this).toggleClass('glyphicon-chevron-down');
+    });
+
+    $('.github').addClass('hidden');
+    setTimeout(function () {
+        $('.github').removeClass('hidden');
+    }, 1000);
+
+    [].forEach.call(document.querySelectorAll('pre'), function($pre) {
+        var lines = $pre.textContent.split('\n');
+        var matches;
+        var indentation = (matches = /^\s+/.exec(lines[0])) != null ? matches[0] : null;
+        if (!!indentation) {
+            lines = lines.map(function(line) {
+                return line.replace(indentation, '');
+            });
+            return $pre.textContent = lines.join('\n').trim();
+        }
+    });
+});
